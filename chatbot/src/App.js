32c77301ba9f7674566c2642c81e74fe8453194c
graha <graham.squires3@gmail.com>
2021-10-09 11:40:23 -0400

    initial commit

---
+++ chatbot/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Chatbot from 'react-chatbot-kit'
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './Config';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
